6fc3f0503fd499c57887d4abdcb647fd97e2b052
eddme-root <ferreiraad3@gmail.com>
2025-09-26 19:40:21 -0400

    AuthContext

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,50 @@
+import React, { createContext, useEffect, useState } from 'react'
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+
+const AuthContext = createContext(null);
+
+export const AuthContextProvider = ({ children }) => {
+
+    const [ user, setUser ] = useState(null);
+    const [ isAdmin, setIsAdmin ] = useState(false);
+
+    // Fetch Admin
+    const fetchAdmin = async ()=> {
+        try {
+            const { data } = await axios.get('/api/admin/is-auth')
+            setIsAdmin(data.success);
+
+        } catch (error) {
+            console.log(error)
+            setIsAdmin(false)
+        }
+    }
+
+    useEffect(()=> {
+        fetchAdmin();
+    }, []);
+
+    const value = {
+        user,
+        setUser,
+        isAdmin,
+        setIsAdmin,
+        axios,
+    }
+
+  return (
+    <AuthContext.Provider value={value}>
+      { children }
+    </AuthContext.Provider>
+  )
+}
+
+AuthContextProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+}
+
+export default AuthContext
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,16 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { GlobalContextProvider } from './context/GlobalContext.jsx'
 import { UIContextProvider } from './context/UIContext.jsx'
+import { AuthContextProvider } from './context/AuthContext.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <GlobalContextProvider>
       <UIContextProvider>
+        <AuthContextProvider>
             <App />
+        </AuthContextProvider>
       </UIContextProvider>  
     </GlobalContextProvider>
   </BrowserRouter>,
